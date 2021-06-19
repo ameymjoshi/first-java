b1df16db74d3c0a2ae1a2bd011d3d48b5d6c0435
Amey Joshi <ameyjoshi@gmail.com>
2021-06-19 16:49:14 +0530

    added div & multi TC

--- first/src/test/java/amey/practice/CalcImplTest.java
+++ first/src/test/java/amey/practice/CalcImplTest.java
@@ -12,18 +12,40 @@ public class CalcImplTest {
     @Test
     public void addTest(){
         assertEquals(10, calc.add(5, 5));
-        assertEquals(20, calc.add(15, 5));
+        assertEquals(20, calc.add(15, 5));        
+        assertEquals(0, calc.add(0, 0));    
+
         assertNotEquals(10, calc.add(15, 5));
-        assertNotEquals(0, calc.add(1, 0));
-        assertEquals(0, calc.add(0, 0));        
+        assertNotEquals(0, calc.add(1, 0));    
     }
 
     @Test
     public void subTest(){
         assertEquals(10, calc.sub(15, 5));
-        assertEquals(-10, calc.sub(10, 20));
+        assertEquals(-10, calc.sub(10, 20));        
+        assertEquals(-5, calc.sub(0, 5)); 
+
         assertNotEquals(-5, calc.sub(15, 5));
         assertNotEquals(0, calc.sub(1, 0));
-        assertEquals(-5, calc.sub(0, 5)); 
+    }
+
+    @Test
+    public void divTest(){
+        assertEquals(3, calc.divide(15, 5));
+        assertEquals(2, calc.divide(20, 10));        
+        assertEquals(0, calc.divide(1, 0)); 
+
+        assertNotEquals(-3, calc.divide(15, 5));
+        assertNotEquals(3, calc.divide(30, 3));
+    }
+
+    @Test
+    public void multiTest(){
+        assertEquals(75, calc.multiply(15, 5));
+        assertEquals(200, calc.multiply(10, 20));        
+        assertEquals(0, calc.multiply(0, 5)); 
+
+        assertNotEquals(-5, calc.multiply(15, 5));
+        assertNotEquals(2, calc.multiply(1, 0));
     }
 }
