c69714ed81d5663ad4a601f2566e61cc97f6d6be
Amey Joshi <ameyjoshi@gmail.com>
2021-06-25 00:40:55 +0530

    Completed puzzle 1. TC success

--- first/src/main/java/amey/puzzles/Puzzle1.java
+++ first/src/main/java/amey/puzzles/Puzzle1.java
@@ -1,6 +1,5 @@
 package amey.puzzles;
 
-import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Puzzle1 {
@@ -10,43 +9,34 @@ public class Puzzle1 {
     A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
     Your task is to write a function that takes a string and return a new string with all vowels removed.
     */
-    static Scanner scan;
     public static void main(String[] args) {
-        
-        scan = new Scanner(System.in);
-        String inputComment = scan.nextLine();
-        String result = replaceVowel(inputComment);
+        Scanner scan = new Scanner(System.in);
+        String result = replaceVowel(scan.nextLine());
         System.out.println(result);
-        //String finalResult = cleanUp(result);
-        //System.out.println(finalResult);
         scan.close();
     }
 
-    private static String replaceVowel(String inputComment) {
-        char[] inputUnderTest = inputComment.toCharArray();
-        StringBuilder sb = new StringBuilder().append(inputComment);
-        for(int counter=0; counter<=(inputComment.length()-1);counter++){
-            if(inputUnderTest[counter]=='a' ||
-                inputUnderTest[counter]=='e' ||
-                inputUnderTest[counter]=='i' ||
-                inputUnderTest[counter]=='o'||
-                inputUnderTest[counter]=='u'){
-                sb.setCharAt(counter,'*');
+    public static String replaceVowel(String nextLine) {
+        char[] inputStr = nextLine.toCharArray();
+        StringBuilder inputStrCopy = new StringBuilder(nextLine);
+        for (int i=0;i<inputStr.length;i++) {
+            if(charMatchesVowel(inputStr[i])){
+                inputStrCopy.deleteCharAt(i);
+                inputStr = inputStrCopy.toString().toCharArray();
+                i--;                
             }
         }
-
-        return sb.toString();
+        return inputStrCopy.toString();
     }
 
-    private static String cleanUp(String result) {
-        StringBuilder sb = new StringBuilder(result);
-        //StringBuilder sb1 = new StringBuilder(result);
-        char[] starString = result.toCharArray();
-        for(int i=0;i<=(sb.length()-1);i++){
-            if(starString[i]=='*'){
-                sb.deleteCharAt(i);
+    private static boolean charMatchesVowel(char c) {
+        if(c=='a' ||
+            c=='e' ||
+            c=='i' ||
+            c=='o' ||
+            c=='u'){
+                return true;
             }
-        }
-        return sb.toString();
+        return false;
     }
 }
--- first/src/test/java/amey/puzzles/Puzzle1Test.java
+++ first/src/test/java/amey/puzzles/Puzzle1Test.java
@@ -1,7 +1,6 @@
 package amey.puzzles;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -9,7 +8,11 @@ public class Puzzle1Test {
     
     @Test
     public void removeVowelTest(){
-        //fail("No meth found.");
-        assertFalse("No impl found", true);
+        assertEquals("my", Puzzle1.replaceVowel("amey"));
+        assertEquals("12345", Puzzle1.replaceVowel("a1e2i3o4u5"));
+        assertEquals("123", Puzzle1.replaceVowel("aaa1eee2iii3ooouuu"));
+        assertEquals("123", Puzzle1.replaceVowel("aeiou123uoiea"));
+        assertEquals("112233", Puzzle1.replaceVowel("11aeiou22aeiou33"));
+        assertEquals("Mry hd  lttl lmb", Puzzle1.replaceVowel("Mary had a little lamb"));
     }
 }
